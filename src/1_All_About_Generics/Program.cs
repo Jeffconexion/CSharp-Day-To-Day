d9963c7b786ab06eaf5e44930a0241161bd622bf
Jefferson Santos <jeff.debian2016@gmail.com>
2022-05-19 06:23:04 -0300

    all about generics.

---
+++ src/1_All_About_Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_All_About_Generics
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+      Console.WriteLine("--------------  Fim  ---------------");
+      Console.ReadKey();
+
+    }
+  }
+}
